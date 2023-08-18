f28e748b9a4d77d436d0f84788b70c47904e4fc1
Kilvny <108346469+Kilvny@users.noreply.github.com>
2023-08-18 15:26:24 +0300

    setup redirects for admin to */dashboard and for user to */invoices

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/nkry-ca",
+        destination: "/nkry-ca/dashboard",
+        permanent: true,
+      },
+      {
+        source: "/workers-manager",
+        destination: "/workers-manager/dashboard",
+        permanent: true,
+      },
+      {
+        source: "/nkry-ca/customer-service",
+        destination: "/nkry-ca/customer-service/invoices",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/nkry-ca/dashboard/Dashboard.tsx
+++ src/app/nkry-ca/dashboard/Dashboard.tsx
@@ -1,11 +1,21 @@
+'use client'
 import { Box, Grid, Paper } from "@mui/material"
 import scss from "./Dashboard.module.scss"
 import PersistentDrawerLeft from "@/components/Sidebar/Drawer/Drawer"
 import DataRibbon from "@/components/Dashboard/DataRibbon/DataRibbon"
 import TransactionsPerDay from "@/components/Dashboard/TransactionsPerDay/TransactionsPerDay"
 import TransactionBottomRow from "@/components/Dashboard/TransactionBottomRow/TransactionBottomRow"
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/navigation"
 
 const Dashboard = () => {
+    const { data: session } = useSession()
+    const router = useRouter()
+
+    if (session?.user?.role === 'user'){
+        router.push("/nkry-ca/customer-service")
+        return
+    }
     return (
         <Box sx={{ p: '1rem 80px', m: '0 1rem 3rem'}}>
           <Grid gap={4} marginTop={2}>
